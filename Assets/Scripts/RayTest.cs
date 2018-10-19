a643f2ababb8f06b128c3bbe8519139d23b772bf
bwsnb <bwsnbb@163.com>
2018-10-19 14:32:41 +0800

    10191432

--- Assets/Scripts/RayTest.cs
+++ Assets/Scripts/RayTest.cs
@@ -4,25 +4,8 @@ using UnityEngine;
 
 public class RayTest : MonoBehaviour {
 
-
-
-<<<<<<< HEAD
-    private void Awake()
-    {
-        Debug.Log("holle world");
-        Debug.Log("holle world");
-    }
-
-
-    // Use this for initialization
-    void Start () {
-=======
-
-
-
 	// Use this for initialization
 	void Start () {
->>>>>>> 8ad939cabe481cad0d2b65a0748e394ac813dd3b
 		
 	}
 	
